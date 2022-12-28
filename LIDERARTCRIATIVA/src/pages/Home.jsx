9bad7afa7c4b68f919500e7a5d4a09ffb56c4758
liderartecriativa <liderartpersonaliza@gmail.com>
2022-12-28 12:27:06 -0300

    ok

--- LIDERARTCRIATIVA/src/pages/Home.jsx
+++ LIDERARTCRIATIVA/src/pages/Home.jsx
@@ -35,7 +35,12 @@ export const Home = () => {
                     </div>
                 </Col>
                 <Col span={24} align={'center'} id="developer-Col">
-
+                <i  id="icon-curly" class="fa-brands fa-connectdevelop fa-5x"></i>
+                    <h5 id="developer-Title">Precisa de desenvolvedores de primeira linha?</h5>
+                    <h4 id="developer-Subtitle">Você está no lugar certo</h4>
+                    <p id="developer-Description">A LiderArt Criativa ajuda empreendedores a construir e lançar novos produtos e,<br /> ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta <br /> para uso e começar a construir.</p>
+                </Col>
+                <Col span={24} align={'center'}>
                     <i id="icon-Html" class="fa-brands fa-html5 fa-3x"></i>
                     <i id="icon-Css" class="fa-brands fa-css3-alt fa-3x"></i>
                     <i id="icon-Bootstrap" class="fa-brands fa-bootstrap fa-3x"></i>
@@ -43,9 +48,6 @@ export const Home = () => {
                     <i id="icon-Vue" class="fa-brands fa-vuejs fa-3x"></i>
                     <i id="icon-Angular" class="fa-brands fa-angular fa-3x"></i>
                     <i id="icon-React" class="fa-brands fa-react fa-3x"></i>
-                    <h5 id="developer-Title">Precisa de desenvolvedores de primeira linha?</h5>
-                    <h4 id="developer-Subtitle">Você está no lugar certo</h4>
-                    <p id="developer-Description">A LiderArt Criativa ajuda empreendedores a construir e lançar novos produtos e,<br /> ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta <br /> para uso e começar a construir.</p>
                 </Col>
             </Row>
         </div>
