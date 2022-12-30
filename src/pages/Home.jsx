5cb52fbe35ae6a6adc10874df2f1c8d670bbdd43
liderartecriativa <76879037+liderartecriativa@users.noreply.github.com>
2022-12-30 00:19:03 +0000

    Alterei para Programas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
                         <img src="src/assets/logo2.png" id="logo" />
                     </div>
                     <div id="title" align={'center'}>
-                        <h1>Desenvolvedores <br /> de Hardwere  de elite
+                        <h1>Desenvolvedores <br /> de Programas  de elite
                             <br /> sob demanda!</h1>
                     </div>
                     <div id="paragraph" align={'center'}>
