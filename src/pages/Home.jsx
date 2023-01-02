ee45e528ec78a176f7a2cacdc2f28fd3d6689ced
liderartecriativa <76879037+liderartecriativa@users.noreply.github.com>
2023-01-02 20:25:19 +0000

    #

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Col, Row } from 'antd';
 import { Button, Space } from 'antd';
+import { useState } from 'react';
 
 //CoreUi Icons
 import CIcon from '@coreui/icons-react';
@@ -10,84 +11,102 @@ import * as icon from '@coreui/icons';
 
 export const Home = () => {
 
+    const [lang, setLang] = React.useState(1);
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setLang((prevLang) => (prevLang >=  8 ? 0 : prevLang + 1));
+
+        }, 3000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
 
     return (
-
-        <div id="content">
-            <Row justify={'center'} gutter={[48, 0]}>
-                <Col flex={'auto'} className="bg">
-                    <div align={'center'}>
-                        <img src="src/assets/logo2.png" id="logo" />
-                    </div>
-                    <div id="title" align={'center'}>
-                        <h1>Desenvolvedores <br /> de Softwere de elite
-                            <br /> sob demanda!</h1>
-                    </div>
-                    <div id="paragraph" align={'center'}>
-                        <p>A maneira mais inteligente possível <br /> de adquirir uma produto de alto desempenho.</p>
-                    </div>
-                    <div align={'center'}>
-                        <Space wrap>
-                            <Button type="primary" id='button-Home' href="https://www.facebook.com" target='_blank'>VAMOS CONVERSAR</Button>
-                        </Space>
-                    </div>
-                    <div align={'center'}>
-                        <img src="src/assets/logo.png" id="logo2" />
-                    </div>
-                </Col>
-
-                <Col span={24} align={'center'} id="developer-Col">
-                    <i id="icon-curly" class="fa-brands fa-connectdevelop fa-5x fa-spin"></i>
-                    <h5 id="developer-Title">Precisa de desenvolvedores de primeira linha?</h5>
-                    <h4 id="developer-Subtitle">Você está no lugar certo</h4>
-                    <p id="developer-Description">A LiderArt Criativa ajuda empreendedores a construir e lançar novos produtos e,<br /> ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta <br /> para uso e começar a construir.</p>
-                </Col>
-                <Row justify={'center'} gutter={[16, 8]} >
-
-                    <Col id="parametros" flex={'auto'} align={'center'} >
-                        <div>
-                            <p id="construcao">Precisa construir sua aplicação?</p>
+        <>
+            <div id="content">
+                <Row justify={'center'} gutter={[48, 0]}>
+                    <Col flex={'auto'} className="bg">
+                        <div align={'center'}>
+                            <img src="src/assets/logo2.png" id="logo" />
                         </div>
-                        <div>
-                            <p id="crescer">Vamos usar as melhores ferramentas para que você possa<br />continuar crescendo imediatamente.<br /><br /> <b>Somos especialistas em:</b></p>
+                        <div id="title" align={'center'}>
+                            <h1>Desenvolvedores <br /> de Softwere de elite
+                                <br /> sob demanda!</h1>
                         </div>
-                        <Col flex={'auto'} align={'center'}>
-                            <div id="margin-Icons">
-                                <Space size={130}>
+                        <div id="paragraph" align={'center'}>
+                            <p>A maneira mais inteligente possível <br /> de adquirir uma produto de alto desempenho.</p>
+                        </div>
+                        <div align={'center'}>
+                            <Space wrap>
+                                <Button type="primary" id='button-Home' href="https://www.facebook.com" target='_blank'>VAMOS CONVERSAR</Button>
+                            </Space>
+                        </div>
+                        <div align={'center'}>
+                            <img src="src/assets/logo.png" id="logo2" />
+                        </div>
+                    </Col>
+
+                    <Col span={24} align={'center'} id="developer-Col">
+                        <i id="icon-curly" class="fa-brands fa-connectdevelop fa-5x fa-spin"></i>
+                        <h5 id="developer-Title">Precisa de desenvolvedores de primeira linha?</h5>
+                        <h4 id="developer-Subtitle">Você está no lugar certo</h4>
+                        <p id="developer-Description">A LiderArt Criativa ajuda empreendedores a construir e lançar novos produtos e,<br /> ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta <br /> para uso e começar a construir.</p>
+                    </Col>
+                    <Row justify={'center'} gutter={[16, 8]} >
+
+                        <Col id="parametros" flex={'auto'} align={'center'} >
+                            <div>
+                                <p id="construcao">Precisa construir sua aplicação?</p>
+                            </div>
+                            <div>
+                                <p id="crescer">Vamos usar as melhores ferramentas para que você possa<br />continuar crescendo imediatamente.<br /><br /> <b>Somos especialistas em:</b></p>
+                            </div>
+                            <Col flex={'auto'} align={'center'}>
+                                {lang == 0 && (
                                     <i id="icon-Html" class="fa-brands fa-html5 fa-3x"></i>
+
+                                )}
+                                {lang == 1 && (
                                     <i id="icon-Css" class="fa-brands fa-css3-alt fa-3x"></i>
+
+                                )}
+                                {lang == 2 && (
                                     <i id="icon-Bootstrap" class="fa-brands fa-bootstrap fa-3x"></i>
-                                </Space>
-                                <Space />
-                            </div>
-                        </Col>
-                        <Col flex={'auto'} align={'center'} >
-                            <div id="margin-Icons">
-                                <Space size={135}>
+                                )}
+                                {lang == 3 && (
                                     <i id="icon-Js" class="fa-brands fa-js fa-3x"></i>
+                                )}
+                                {lang == 4 && (
                                     <i id="icon-Vue" class="fa-brands fa-vuejs fa-3x"></i>
+                                )}
+                                {lang == 5 && (
                                     <i id="icon-Angular" class="fa-brands fa-angular fa-3x"></i>
-                                </Space>
-                            </div>
-                        </Col>
-                        <Col flex={'auto'} align={'center'} >
-                            <div id="margin-Icons">
-                                <Space size={122}>
+                                )}
+                                {lang == 6 && (
                                     <i id="icon-React" class="fa-brands fa-react fa-3x"></i>
+                                )}
+                                {lang == 7 && (
                                     <CIcon icon={icon.cibNextJs} width={70} height={45} />
+                                )}
+                                {lang == 8 && (
                                     <i id="icon-Github" class="fa-brands fa-github  fa-3x"></i>
-                                </Space>
-                            </div>
+                                )}
+                            </Col>
+                            <Col flex={'auto'} align={'center'}>
+
+
+
+                            </Col>
                         </Col>
-                    </Col>
-
-                    <Col id="parametros" flex={'auto'} align={'center'}>
-
-                      
-                    </Col>
+                    </Row>
                 </Row>
-            </Row>
-        </div>
+            </div>
+        </>
+
+
 
     );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,10 @@
+
 export const Products = () => {
 
 
     return (
         <div id="content">
-
-            <p>EU SOU O PRODUTO</p>
+           
         </div>
     );
 }
