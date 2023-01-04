2ac91e6e4c8ee9b83f5d17c3402c27bb387a1765
liderartecriativa <76879037+liderartecriativa@users.noreply.github.com>
2023-01-04 14:22:34 +0000

    #

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,6 @@ import React from 'react'
 import { Col, Row } from 'antd';
 import { Button, Space } from 'antd';
 
-//CoreUi Icons
-import CIcon from '@coreui/icons-react';
-import * as icon from '@coreui/icons';
-
 
 
 export const Home = () => {
@@ -15,7 +11,7 @@ export const Home = () => {
 
     React.useEffect(() => {
         const timer = setInterval(() => {
-            setLang((prevLang) => (prevLang >=  8 ? 0 : prevLang + 1));
+            setLang((prevLang) => (prevLang >= 7 ? 0 : prevLang + 1));
 
         }, 3000);
         return () => {
@@ -24,12 +20,12 @@ export const Home = () => {
     }, []);
 
     //ZERA O PADDING E MARGIN DAS COLUNAS NECESSARIAS
-    const paddingCol = {
-        padding: '0px',
-        margin: '0px',
-        
-    }   
-
+    
+    const colorColHtml = {
+        padding: '0px', margin: '0px',
+        background:'#ff531f',
+    }
+    
     return (
         <>
 
@@ -61,22 +57,22 @@ export const Home = () => {
                         <h4 id="developer-Subtitle">Você está no lugar certo</h4>
                         <p id="developer-Description">A LiderArt Criativa ajuda empreendedores a construir e lançar novos produtos e,<br /> ajuda as empresas em crescimento conectando equipes de desenvolvimento pronta <br /> para uso e começar a construir.</p>
                     </Col>
-                    <Col id="parametros" span={24} align={'center'} style={paddingCol}>
+                    <Col id="parametros" span={24} align={'center'} style={colorColHtml} >
                         <div className="lines">
                             <div className="line"></div>
                             <div></div>
                             <div className="line"></div>
-                            <Col span={12} align={'center'}>
-                            <div>
-                                <p id="construcao">Precisa construir sua aplicação?</p>
-                            </div>
-                            <div>
-                                <p id="crescer">Vamos usar as melhores ferramentas para que você possa<br />continuar crescendo imediatamente.<br /><br /> <b>Somos especialistas em:</b></p>
-                            </div>
+                            <Col span={12} align={'center'} >
+                                <div>
+                                    <p id="construcao">Precisa construir sua aplicação?</p>
+                                </div>
+                                <div>
+                                    <p id="crescer">Vamos usar as melhores ferramentas para que você possa<br />continuar crescendo imediatamente.<br /></p>
+                                </div>
                             </Col>
                             {lang == 0 && (
                                 <Row>
-                                    <Col flex={'auto'} id="html" align={'center'}>
+                                    <Col flex={'auto'} id="html" align={'center'}style={colorColHtml} >
                                         <i id="icon-Html" class="fa-brands fa-html5 fa-10x"></i>
                                         <Col span={12}>
                                             <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
@@ -86,7 +82,7 @@ export const Home = () => {
                             )}
                             {lang == 1 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="css" align={'center'}>
+                                    <Col flex={'auto'} id="css" align={'center'}>
                                         <i id="icon-Css" class="fa-brands fa-css3-alt fa-10x"></i>
                                         <Col span={12}>
                                             <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
@@ -96,7 +92,7 @@ export const Home = () => {
                             )}
                             {lang == 2 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="bootstrap" align={'center'}>
+                                    <Col flex={'auto'} id="bootstrap" align={'center'}>
                                         <i id="icon-Bootstrap" class="fa-brands fa-bootstrap fa-10x"></i>
                                         <Col span={12}>
                                             <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
@@ -106,61 +102,52 @@ export const Home = () => {
                             )}
                             {lang == 3 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="js" align={'center'}>
+                                    <Col flex={'auto'} id="js" align={'center'}>
                                         <i id="icon-Js" class="fa-brands fa-js fa-10x"></i>
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
+                                        <Col span={12}>
+                                            <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
+                                        </Col>
                                     </Col>
                                 </Row>
                             )}
                             {lang == 4 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="vue" align={'center'}>
+                                    <Col flex={'auto'} id="vue" align={'center'}>
                                         <i id="icon-Vue" class="fa-brands fa-vuejs fa-10x"></i>
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
+                                        <Col span={12}>
+                                            <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
+                                        </Col>
                                     </Col>
                                 </Row>
                             )}
                             {lang == 5 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="angular" align={'center'}>
+                                    <Col flex={'auto'} id="angular" align={'center'}>
                                         <i id="icon-Angular" class="fa-brands fa-angular fa-10x"></i>
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
+                                        <Col span={12}>
+                                            <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
+                                        </Col>
                                     </Col>
                                 </Row>
                             )}
                             {lang == 6 && (
                                 <Row>
-                                    <Col  flex={'auto'} id="react" align={'center'}>
+                                    <Col flex={'auto'} id="react" align={'center'}>
                                         <i id="icon-React" class="fa-brands fa-react fa-10x"></i>
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
+                                        <Col span={12}>
+                                            <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
+                                        </Col>
                                     </Col>
                                 </Row>
                             )}
+                            
                             {lang == 7 && (
-                                <Row>
-                                    <Col flex={'auto'}  id="nextjs" align={'center'}>
-                                        <CIcon icon={icon.cibNextJs} width={150} height={150} style={paddingCol} />
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
-                                    </Col>
-                                </Row>
-                            )}
-                            {lang == 8 && (
                                 <Row>
                                     <Col flex={'auto'} id="github" align={'center'}>
                                         <i class="fa-brands fa-github  fa-10x"></i>
-                                    <Col span={12}>
-                                        <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
-                                    </Col>
+                                        <Col span={12}>
+                                            <h5>HTML5, sigla para Hypertext Markup Language, é uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web.</h5>
+                                        </Col>
                                     </Col>
                                 </Row>
                             )}
